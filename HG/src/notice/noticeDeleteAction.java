c226cb407bbf61e42a11c8b7e002c51b5ee4d5e3
신희진 <user1@gmail.com>
2017-05-08 15:13:46 +0900

    히진

--- HG/src/notice/noticeDeleteAction.java
+++ HG/src/notice/noticeDeleteAction.java
@@ -1,28 +1,52 @@
 package notice;
 
-import java.io.IOException;
 import java.io.Reader;
-import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+
+import member.memberVO;
 
 import com.ibatis.common.resources.Resources;
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class noticeDeleteAction extends ActionSupport{
-	static private Reader reader;
-	static private SqlMapClient sqlMapper;
+		public static Reader reader; // 파일 스트림을 위한 reader
+		public static SqlMapClient sqlMapper;// SqlMapClient API를 사용하기 위함
 	
-	private noticeVO paramClass;
+		private memberVO memresultClass;
+		private memberVO memparamClass;
+		private noticeVO resultClass;
+		private noticeVO paramClass;
+		private int currentPage;
+		
+		private int n_number;
+		private String n_title;
+		private String n_name;
+		private String n_content;
 	
-	public noticeDeleteAction() throws IOException{
+	public noticeDeleteAction() throws Exception{
 		reader=Resources.getResourceAsReader("sqlMapConfig.xml");
 		sqlMapper=SqlMapClientBuilder.buildSqlMapClient(reader);
 		reader.close();
 	}
 	
-	public String execute() throws SQLException{
-		sqlMapper.delete("Board.deleteNotice", paramClass);
+	public String execute() throws Exception{
+		memparamClass = new memberVO();
+		memresultClass = new memberVO();
+		paramClass = new noticeVO();
+		resultClass = new noticeVO();
+		/*ActionContext context = ActionContext.getContext();
+		Map<String, Object> session = context.getSession();
+		String session_id = (String) session.get("m_id");
+		memresultClass = (memberVO) sqlMapper.queryForObject("Member.UserCheck",
+				session_id);*/
+		paramClass.setN_number(getN_number());
+		System.out.println(n_number);
+		sqlMapper.delete("Board.deleteNotice",paramClass);
 		return SUCCESS;
 	}
 
@@ -42,6 +66,30 @@ public class noticeDeleteAction extends ActionSupport{
 		noticeDeleteAction.sqlMapper = sqlMapper;
 	}
 
+	public memberVO getMemresultClass() {
+		return memresultClass;
+	}
+
+	public void setMemresultClass(memberVO memresultClass) {
+		this.memresultClass = memresultClass;
+	}
+
+	public memberVO getMemparamClass() {
+		return memparamClass;
+	}
+
+	public void setMemparamClass(memberVO memparamClass) {
+		this.memparamClass = memparamClass;
+	}
+
+	public noticeVO getResultClass() {
+		return resultClass;
+	}
+
+	public void setResultClass(noticeVO resultClass) {
+		this.resultClass = resultClass;
+	}
+
 	public noticeVO getParamClass() {
 		return paramClass;
 	}
@@ -49,5 +97,46 @@ public class noticeDeleteAction extends ActionSupport{
 	public void setParamClass(noticeVO paramClass) {
 		this.paramClass = paramClass;
 	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public int getN_number() {
+		return n_number;
+	}
+
+	public void setN_number(int n_number) {
+		this.n_number = n_number;
+	}
+
+	public String getN_title() {
+		return n_title;
+	}
+
+	public void setN_title(String n_title) {
+		this.n_title = n_title;
+	}
+
+	public String getN_name() {
+		return n_name;
+	}
+
+	public void setN_name(String n_name) {
+		this.n_name = n_name;
+	}
+
+	public String getN_content() {
+		return n_content;
+	}
+
+	public void setN_content(String n_content) {
+		this.n_content = n_content;
+	}
 	
-}
+
+	}
